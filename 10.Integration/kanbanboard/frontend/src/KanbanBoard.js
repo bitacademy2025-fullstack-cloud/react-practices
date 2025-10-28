0b82e3e8b414972e0d0b5607815c504ee03411fb
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-10-28 14:48:33 +0900

    kanbanboard #1

---
+++ 10.Integration/kanbanboard/frontend/src/KanbanBoard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import data from './assets/json/data';
+
+function KanbanBoard () {
+    
+    console.log(data);
+
+    return (
+        <div>
+            <h1>KanbanBoard</h1>
+        </div>
+    );
+}
+
+export default KanbanBoard;

ff5ca35717afe43f1b570ac233600662023faae2
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-10-28 16:41:29 +0900

    kanbanboard #1

--- 10.Integration/kanbanboard/frontend/src/KanbanBoard.js
+++ 10.Integration/kanbanboard/frontend/src/KanbanBoard.js
@@ -1,24 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import CardList from './CardList.js';
-import './assets/scss/KanbanBoard.scss';
 import data from './assets/json/data.js';
+
+const StyledDiv = styled.div`
+    white-space: nowrap;
+    height: 100%;
+    margin: 20px auto;
+`;
 
 const KanbanBoard = () => {
     return (
-            <div className={'Kanban_Board'}>
+            <StyledDiv className={'KanbanBoard'}>
                 <CardList 
                     key={'To Do'}
                     title={'To Do'}
-                    cards={cards.filter(card => card.status === 'ToDo')} />
+                    cards={data.filter(card => card.status === 'ToDo')} />
                 <CardList
                     key={'Doing'}
                     title={'Doing'}
-                    cards={cards.filter(card => card.status === 'Doing')} />
+                    cards={data.filter(card => card.status === 'Doing')} />
                 <CardList
                     key={'Done'}
                     title={'Done'}
-                    cards={cards.filter(card => card.status === 'Done')} />
-                </div>
+                    cards={data.filter(card => card.status === 'Done')} />
+            </StyledDiv>
     );
 };
 

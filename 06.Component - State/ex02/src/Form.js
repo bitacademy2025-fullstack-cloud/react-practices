8bf3cfa9b154d278e419105eeae0aaed9c026143
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-10-29 16:56:41 +0900

    state #3

--- 06.Component - State/ex02/src/Form.js
+++ 06.Component - State/ex02/src/Form.js
@@ -5,7 +5,7 @@ export default function Form() {
     const [name, setName] = useState();
     const [email, setEmail] = useState('');
     const [validEmail, setValisEmail] = useState(false);
-
+    const [agreeProvChecked, setAgreeProvChecked] = useState(false);
     return (
         <form
             id="joinForm"
@@ -83,7 +83,16 @@ export default function Form() {
 
             <fieldset>
                 <legend>약관동의</legend>
-                <input id="agree-prov" type="checkbox" name="agreeProv" value= { "yes" } defaultChecked={ false } />
+                <input
+                    id="agree-prov"
+                    type="checkbox"
+                    name="agreeProv"
+                    value = {"yes"}
+                    checked = {agreeProvChecked}
+                    onChange = {(e) => {
+                        setAgreeProvChecked(!agreeProvChecked);
+                    }}
+                    />
                 <label>서비스 약관에 동의합니다.</label>
             </fieldset>
 

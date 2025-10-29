9d8597aac9359ca8baf6f7eb046857f82e060c72
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-10-29 17:02:58 +0900

    kanbanboard #2

--- 10.Integration/kanbanboard/frontend/src/Card.js
+++ 10.Integration/kanbanboard/frontend/src/Card.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TaskList from './TaskList';
 import {_Card, Card_Title, Card_Title_Open}from './assets/scss/Card.scss';
 
 const Card = ({title, description, tasks}) => {
+    const [showDetails, setShowDetails] = useState(true);
 
     return (
         <div className={_Card}>
             <div
                 className={[Card_Title, Card_Title_Open].join(' ')}>
                 {title}
-            </div>   
+            </div>
+
             <div>
                 {description}
                 <TaskList tasks={tasks} />

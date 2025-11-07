80234897dc0d694a761f20a5bf7e7e959b1ff0e8
bitacademy2025-fullstack-cloud <bitacademy2025.fullstack.cloud@gmail.com>
2025-11-07 15:46:25 +0900

    Update Card.js

--- 10.Integration/kanbanboard/frontend/src/Card.js
+++ 10.Integration/kanbanboard/frontend/src/Card.js
@@ -2,15 +2,110 @@ import React, {useState} from 'react';
 import TaskList from './TaskList';
 import {_Card, Card_Title, Card_Title_Open}from './assets/scss/Card.scss';
 
-const Card = ({title, description, tasks}) => {
+const Card = ({no, title, description, status}) => {
     const [showDetails, setShowDetails] = useState(false);
+    const [tasks, setTasks] = useState([]);
 
+    const changeTaskDone = async (no, done) => {
+        try {
+            const response = await fetch(`/api/task/${no}`, {
+                method: 'put',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Accept': 'application/json'
+                },
+                body: `done=${done}`
+            });
+
+            if (!response.ok) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+
+            const json = await response.json();
+            if (json.result !== 'success') {
+                throw new Error(`${json.result} ${json.message}`);
+            }
+            
+            const newTasks = update(tasks, {
+                [tasks.findIndex(task => task.no === json.data.no)]: {
+                    done: {
+                        $set: json.data.done
+                    }
+                }
+            });
+
+            setTasks(newTasks);
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const addTask = async (taskName) => {
+        try {
+            const newTask = {
+                no: null,
+                name: taskName,
+                done: 'N',
+                cardNo: no
+            };
+
+            const response = await fetch(`/api/task`, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(newTask)
+            });
+
+            if (!response.ok) {
+                throw new Error(`${response.status} ${response.statusText}`);
+            }
+
+            const json = await response.json();
+            if (json.result !== 'success') {
+                throw new Error(`${json.result} ${json.message}`);
+            }
+
+            setTasks([json.data, ...tasks]);
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    
     return (
         <div className={_Card}>
             <div
                 className={[Card_Title, (showDetails ? Card_Title_Open : '')].join(' ')}
-                onClick={() => {
-                   setShowDetails(!showDetails);
+                onClick={async () => {
+                    if(!showDetails) {
+                        try {
+                            const response = await fetch(`/api/task?cardNo=${no}`, {
+                                method: 'get',
+                                headers: {
+                                    'Accept': 'application/json'
+                                }
+                            });
+                
+                            if (!response.ok) {
+                                throw new Error(`${response.status} ${response.statusText}`);
+                            }
+                
+                            const json = await response.json();
+                            if (json.result !== 'success') {
+                                throw new Error(`${json.result} ${json.message}`);
+                            }
+                
+                            setTasks(json.data);
+
+                        } catch (err) {
+                            console.log(err.message);
+                        }
+                    }
+
+                    setShowDetails(!showDetails);
                 }}>
                 {title}
             </div>
@@ -18,7 +113,11 @@ const Card = ({title, description, tasks}) => {
                 showDetails ?    
                     <div>
                         {description}
-                        <TaskList tasks={tasks} />
+                        <TaskList
+                            cardNo={no}
+                            tasks={tasks}
+                            callbackAddTask={addTask}
+                            callbackChangeTaskDone={changeTaskDone} />
                     </div> :
                     null
             }
